27f81d0b4fdb8a442f6918bdacff17314e0392a4
Billytheegoat <billytheegoat@Billytheegoats-MacBook-Pro.local>
2026-04-02 14:30:30 +0300

    postman test endpoints

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
-from app.routers import client, errands, delivery_partner
+from app.routers import client, errands, delivery_partner, shared_auth
 
 master_router = APIRouter()
 
 master_router.include_router(client.router)
 master_router.include_router(errands.router)
 master_router.include_router(delivery_partner.router)
+master_router.include_router(shared_auth.create_auth_router)
